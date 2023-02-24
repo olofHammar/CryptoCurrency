b9e7b21dd7611efa2a149e0f970f1d98dc98dbb8
Olof Hammar <olof.hammar@shortcut.no>
2023-02-24 09:43:14 +0100

    Initial commit

---
+++ CryptoCurrency/CryptoCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyApp.swift
+//  CryptoCurrency
+//
+//  Created by Olof Hammar on 2023-02-24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
